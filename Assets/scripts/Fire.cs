29252781612856164ee2242696beadfb9643ae28
Randy-122 <1223645455@qq.com>
2026-06-09 16:07:10 +0800

    6.9

--- Assets/scripts/Fire.cs
+++ Assets/scripts/Fire.cs
@@ -1,40 +1,29 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEditor;
 using UnityEngine;
 
 public class Gun : MonoBehaviour
 {
-    // Start is called before the first frame update
     public Rigidbody2D rocket;
     public float speed = 15;
     [SerializeField] public PlayerControl playerCtrl;
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
     void Update()
     {
         if (playerCtrl != null)
         {
             if (Input.GetButtonDown("Fire1"))
-            //if(Input.GetKeyDown(KeyCode.mouse0))
             {
                 if (playerCtrl.bFaceRight)
                 {
-                    Rigidbody2D rocketInstance = Instantiate(rocket, transform.position
-                                                    , Quaternion.Euler(new Vector3(0, 0, 0)));
-                    rocketInstance.velocity = new Vector2(speed, 0);
+                    Rigidbody2D rocketInstance = Instantiate(rocket, transform.position, Quaternion.Euler(new Vector3(0, 0, 0)));
+                    // 使用 transform.right 代替 Vector2(speed, 0)
+                    rocketInstance.velocity = transform.right * speed;
                 }
                 else
                 {
-                    Rigidbody2D rocketInstance = Instantiate(rocket, transform.position
-                                                    , Quaternion.Euler(new Vector3(0, 0, 180)));
-                    rocketInstance.velocity = new Vector2(-speed, 0);
+                    Rigidbody2D rocketInstance = Instantiate(rocket, transform.position, Quaternion.Euler(new Vector3(0, 0, 180)));
+                    // 使用 -transform.right 代替 Vector2(-speed, 0)
+                    rocketInstance.velocity = -transform.right * speed;
                 }
-
             }
         }
     }
