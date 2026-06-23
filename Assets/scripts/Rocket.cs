d39a5b2e7e6db14329d1c0cdd38acb44fd34b639
Randy-122 <1223645455@qq.com>
2026-06-23 21:33:25 +0800

    623

--- Assets/scripts/Rocket.cs
+++ Assets/scripts/Rocket.cs
@@ -23,6 +23,11 @@ public class Rocket : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        //伤害敌人
+        if (collision.CompareTag("Enemy"))
+        {
+            collision.GetComponent<Enemy>().Hurt();
+        }
         if (collision.CompareTag("Player"))
         {
             return;
