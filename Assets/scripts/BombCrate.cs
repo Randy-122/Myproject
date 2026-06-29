394c6c7bbd826bfcc734ed30bc305b3ab9fd7aab
Randy-122 <1223645455@qq.com>
2026-06-29 16:51:59 +0800

    完成医疗弹药包 （无生成）

---
+++ Assets/scripts/BombCrate.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombCrate : MonoBehaviour
+{
+
+    public AudioClip pickupClip;
+    private Animator anim;      		
+    private bool landed = false;
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (anim == null) 
+        {
+            anim = GetComponent<Animator>();
+        }
+        if (other.tag == "Player")
+        {
+            AudioSource.PlayClipAtPoint(pickupClip, transform.position);
+            //other.GetComponent<LayBombs>().bombCount++;//
+            Destroy(transform.root.gameObject);
+        }
+        else if (other.tag == "Ground" && !landed)
+        {
+            anim.SetTrigger("land");
+            transform.parent = null;
+            gameObject.AddComponent<Rigidbody2D>();
+            landed = true;
+        }
+
+
+    }
+}
