d0cd9c8c7cce783b56536ced4bbc92b46035e33c
Randy-122 <1223645455@qq.com>
2026-07-03 16:26:29 +0800

    部分音效替换 水花

--- Assets/scripts/Destoryer.cs
+++ Assets/scripts/Destoryer.cs
@@ -7,7 +7,7 @@ public class Destoryer : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Destroy(transform.gameObject);
+        Destroy(transform.gameObject, 0.5f);
     }
 
     // Update is called once per frame
