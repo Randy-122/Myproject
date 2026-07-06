00d082f81016637c6ba024f11de4a0f4bb3c130e
Randy-122 <1223645455@qq.com>
2026-07-06 14:17:24 +0800

    暂停静音

--- Assets/scripts/MusicManager.cs
+++ Assets/scripts/MusicManager.cs
@@ -8,6 +8,10 @@ public class MusicMixer : MonoBehaviour
 
 
     public AudioMixer audioMixer;
+
+
+    private float savedVolume;
+    private bool isPaused = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -37,6 +41,21 @@ public class MusicMixer : MonoBehaviour
                 audioMixer.SetFloat("mainVolume", volume);
             }   
 
+        }
+
+
+        if (Time.timeScale == 0 && !isPaused)
+        {
+            // 刚进入暂停状态：保存当前音量并静音
+            audioMixer.GetFloat("mainVolume", out savedVolume);
+            audioMixer.SetFloat("mainVolume", -80f); // -80 通常作为静音阈值
+            isPaused = true;
+        }
+        else if (Time.timeScale != 0 && isPaused)
+        {
+            // 刚恢复游戏状态：将音量恢复为暂停前的值
+            audioMixer.SetFloat("mainVolume", savedVolume);
+            isPaused = false;
         }
     }
 }
